26d83e7972dffa06ccc593cc6e15c52ed410a10c
diena1dev <diena1dev@gmail.com>
2026-07-13 18:47:04 -0500

    add copper machine casings and casing variants

--- src/main/kotlin/features/custom/blocks/AnionBlocks.kt
+++ src/main/kotlin/features/custom/blocks/AnionBlocks.kt
@@ -2,7 +2,13 @@ package dev.diena.anion.features.custom.blocks
 
 import dev.diena.anion.data.registry.AnionRegistryKey
 import dev.diena.anion.data.registry.registries.AnionRegistries
+import dev.diena.anion.extensions.gradient
 import dev.diena.anion.features.custom.items.AnionItems
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.Style
+import net.kyori.adventure.text.format.TextColor
+import net.kyori.adventure.util.RGBLike
+import org.bukkit.Color
 import org.bukkit.Instrument
 
 object AnionBlocks {
@@ -42,9 +48,64 @@ object AnionBlocks {
 		)
 	)
 
+	private val COPPER_TEXT_START = TextColor.color(232, 144, 121)
+	private val COPPER_TEXT_END   = TextColor.color(125, 74, 54)
+
+	// copper machine casings
+	val COPPER_MACHINE_CASING = registerBlock(
+		AnionBlock(
+			"Copper Machine Casing",
+			Instrument.ZOMBIE,
+			4,
+			styledDisplayName = Component.text("Copper Machine Casing")
+				.gradient(COPPER_TEXT_START, COPPER_TEXT_END)
+		)
+	)
+
+	// TODO: AnionDirectionalBlock
+	val COPPER_MACHINE_DISPLAY = registerBlock(
+		AnionBlock(
+			"Copper Machine Display",
+			Instrument.ZOMBIE,
+			5,
+			styledDisplayName = Component.text("Copper Machine ")
+				.gradient(COPPER_TEXT_START, COPPER_TEXT_END)
+				.append(Component.text("Display").color(TextColor.color(Color.LIME.asARGB())))
+		)
+	)
+
+	val COPPER_MACHINE_VALVE = registerBlock(
+		AnionBlock(
+			"Copper Machine Valve",
+			Instrument.ZOMBIE,
+			6,
+			styledDisplayName = Component.text("Copper Machine Valve")
+				.gradient(COPPER_TEXT_START, COPPER_TEXT_END)
+		)
+	)
+
+	val COPPER_MACHINE_BUS = registerBlock(
+		AnionBlock(
+			"Copper Machine Bus",
+			Instrument.ZOMBIE,
+			7,
+			styledDisplayName = Component.text("Copper Machine Bus")
+				.gradient(COPPER_TEXT_START, COPPER_TEXT_END)
+		)
+	)
+
+	val COPPER_MACHINE_DATAPORT = registerBlock(
+		AnionBlock(
+			"Copper Machine DataPort",
+			Instrument.ZOMBIE,
+			8,
+			styledDisplayName = Component.text("Copper Machine DataPort")
+				.gradient(COPPER_TEXT_START, COPPER_TEXT_END)
+		)
+	)
+
 	fun fromState(instrument: Instrument, note: Int): AnionBlock? = byState[instrument to note]
 
-	@Suppress("SameParameterValue")
 	private fun registerBlock(block: AnionBlock): AnionBlock {
 		val key = block.instrument to block.note
 		byState[key] = block
